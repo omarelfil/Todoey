47dd3523f3190babdd9917d801f3bcd95fb49ce3
Omar <omar.elfil@gmail.com>
2018-02-21 19:50:28 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Omar El-Fil on 21/02/2018.
+//  Copyright © 2018 Omar El-Fil. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
