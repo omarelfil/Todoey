19eae323e40d1be49cab29d001bce958b1af0581
Omar <omar.elfil@gmail.com>
2018-03-13 14:33:43 +0100

    Persisting data using Realm.

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,17 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Omar El-Fil on 08/03/2018.
+//  Copyright © 2018 Omar El-Fil. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
+
+
--- Todoey/Data Model/Item.swift
+++ Todoey/Data Model/Item.swift
@@ -2,13 +2,16 @@
 //  Item.swift
 //  Todoey
 //
-//  Created by Omar El-Fil on 21/02/2018.
+//  Created by Omar El-Fil on 08/03/2018.
 //  Copyright © 2018 Omar El-Fil. All rights reserved.
 //
 
 import Foundation
+import RealmSwift
 
-class Item: Codable {
-    var title: String = ""
-    var done: Bool = false
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+    @objc dynamic var dateCreated: Date?
+    var parentCategory = LinkingObjects(fromType: Category.self, property: "items")
 }
